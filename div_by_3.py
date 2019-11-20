1fb98a19c4b2887b8f51f3dfce195c75d69481ba
Joshie Villa <57549673+Villanatics@users.noreply.github.com>
2019-11-20 20:21:08 +0800

    Delete div_by_3.py

--- div_by_3.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-b=np.arange(1,101).reshape(10,10)
-A=b*b
-c=b[b%3==0]
-print('The 10x10 squares matrix: ', A)
-print('The elements that are divisible by 3: ',c)
-np.save('div_by_3',A,c)
